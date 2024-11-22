dbf80519a29b3e28327c6b1a1e42df5708cae42e
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-11-22 23:39:15 +0200

    Adding context data

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -2,7 +2,10 @@ import React, { createContext } from 'react';
 
 export const AuthContext=createContext(null)
 const AuthProvider = () => {
-
+    const contextData={
+        name:'mahin',
+        age:20
+    }
     return (
         <div>
             <h1>Auth P</h1>
