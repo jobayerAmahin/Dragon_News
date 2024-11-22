0c886addb53b769f9801c588b7871bdfb5031665
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-11-22 23:35:52 +0200

    Adding context api

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { createContext } from 'react';
 
+export const AuthContext=createContext(null)
 const AuthProvider = () => {
+
     return (
         <div>
             <h1>Auth P</h1>
+            <AuthContext.Provider value={contextData}>
+
+            </AuthContext.Provider>
         </div>
     );
 };
